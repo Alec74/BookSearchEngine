fb06b48fe969b173d9f1974455fe556ec9686dd8
Alec74 <alecmcglone@gmail.com>
2021-09-01 16:57:40 -0400

    server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,32 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
-
 const db = require('./config/connection');
+const routes = require('./routes');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 
-const PORT = process.env.PORT || 3001;
+// Setup to use apollo server on the back-end.
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
+let server;
+async function startApolloServer() {
+  server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  playground: true
 });
 
-server.applyMiddleware({ app });
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+startApolloServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,11 +34,12 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
