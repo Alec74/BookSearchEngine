760a75d2183dfce5a5147e25bdaa69f093e68426
Alec74 <alecmcglone@gmail.com>
2021-09-01 17:15:45 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -1,32 +1,35 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
-
-// Setup to use apollo server on the back-end.
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
-const app = express();
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 let server;
 async function startApolloServer() {
   server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-  playground: true
-});
-
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+    playground: true
+  });
   await server.start();
   server.applyMiddleware({ app });
 }
 
-startApolloServer()
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
+
+// server.applyMiddleware({ app });
+startApolloServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -34,12 +37,11 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
